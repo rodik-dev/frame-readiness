9a6f8bf8da101db925f2d03d9b4c19f5aea38118
Stackbit Code Editor <projects@stackbit.com>
2021-07-13 14:26:12 +0000

    page.js: changed

--- src/layouts/page.js
+++ src/layouts/page.js
@@ -17,6 +17,7 @@ export default class Page extends React.Component {
                 <article className="page py-5 py-sm-6">
                     <div className="container container--medium">
                         {title && <header className="page__header"><h1 className="page__title">{title}</h1></header>}
+                        hello world
                         {markdownContent && <div className="page__body text-block">{markdownify(markdownContent)}</div>}
                     </div>
                 </article>
